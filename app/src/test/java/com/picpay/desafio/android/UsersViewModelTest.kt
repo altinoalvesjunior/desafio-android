a956c4b4198044831510f8af5e046266cc32f771
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2022-08-24 17:48:45 -0300

    TEST: adding unit tests

--- app/src/test/java/com/picpay/desafio/android/UsersViewModelTest.kt
+++ app/src/test/java/com/picpay/desafio/android/UsersViewModelTest.kt
@@ -1,22 +1,25 @@
 package com.picpay.desafio.android
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
-import com.nhaarman.mockitokotlin2.doReturn
+
+import org.mockito.junit.MockitoJUnitRunner
+import org.junit.runner.RunWith
+import junit.framework.Assert.assertEquals
+import org.junit.rules.TestRule
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.mockito.Mock
-import org.junit.rules.TestRule
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
-
-import com.picpay.desafio.android.viewmodel.users.UsersViewModel
-import com.picpay.desafio.android.data.repository.UserRepository
-import com.picpay.desafio.android.model.User
-import com.picpay.desafio.android.viewmodel.users.UsersDataResult
-import junit.framework.Assert.assertEquals
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+
+import com.picpay.desafio.android.model.User
+import com.picpay.desafio.android.data.repository.UserRepository
+import com.picpay.desafio.android.viewmodel.users.UsersDataResult
+import com.picpay.desafio.android.viewmodel.users.UsersViewModel
+
+import androidx.lifecycle.MutableLiveData
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class UsersViewModelTest {
@@ -33,5 +36,33 @@ class UsersViewModelTest {
     fun setup() {
         viewModel = UsersViewModel(userRepository)
     }
-}
 
+   @Test
+    fun `should be return a user`() {
+        suspend fun returnUserTest() = runBlocking {
+
+            val usuario: List<User> =
+                listOf(User(img = "", name = "name", id = 1, username = "username"))
+
+            val users = MutableLiveData<UsersDataResult>()
+            viewModel.getUsers()
+
+            doReturn(usuario).`when`(userRepository).getUserFromDataSource()
+
+            assertEquals(usuario, users.value)
+        }
+    }
+
+    @Test
+    fun `should be return an error`() = runBlocking {
+
+        val returnErrorEnabled: UsersDataResult.ReturnError = UsersDataResult.ReturnError(error = true)
+
+        val users = MutableLiveData<UsersDataResult>()
+        viewModel.getUsers()
+
+        doReturn(returnErrorEnabled).`when`(userRepository).getUserFromDataSource()
+
+        assertEquals(returnErrorEnabled, users.value)
+    }
+}
